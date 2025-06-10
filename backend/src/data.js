eae7ff8460ce24b4adce9b61b507a545683eae4c
Diego <107651797+1Skiy@users.noreply.github.com>
2025-06-09 20:18:33 -0400

    Proyecto

---
+++ backend/src/data.js
@@ -0,0 +1,98 @@
+export const sample_foods = [
+  {
+    
+    name: 'Pizza',
+    cookTime: '10-20',
+    price: 10,
+    favorite: false,
+    origins: ['italy'],
+    stars: 4.5,
+    imageUrl: 'food-1.jpg',
+    tags: ['Comida rapida', 'Pizza', 'Lunch'],
+  },
+  {
+   
+    name: 'Albondigas',
+    price: 20,
+    cookTime: '20-30',
+    favorite: true,
+    origins: ['persia', 'middle east', 'china'],
+    stars: 5,
+    imageUrl: 'food-2.jpg',
+    tags: ['Comida lenta', 'Lunch'],
+  },
+  {
+    
+    name: 'Hamburguesa',
+    price: 5,
+    cookTime: '10-15',
+    favorite: false,
+    origins: ['Aleman', 'us'],
+    stars: 3.5,
+    imageUrl: 'food-3.jpg',
+    tags: ['Comida rapida', 'Hamburguesa'],
+  },
+  {
+   
+    name: 'Papas fritas',
+    price: 2,
+    cookTime: '15-20',
+    favorite: true,
+    origins: ['belgica', 'francia'],
+    stars: 3,
+    imageUrl: 'food-4.jpg',
+    tags: ['Comida rapida', 'Frito'],
+  },
+  {
+   
+    name: 'Sopa de pollo',
+    price: 11,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['india', 'asia'],
+    stars: 3.5,
+    imageUrl: 'food-5.jpg',
+    tags: ['Comida lenta', 'Sopa'],
+  },
+  {
+   
+    name: 'Pizza de vegetales',
+    price: 9,
+    cookTime: '40-50',
+    favorite: false,
+    origins: ['italia'],
+    stars: 4.0,
+    imageUrl: 'food-6.jpg',
+    tags: ['Comida rapida', 'Pizza', 'Lunch'],
+  },
+];
+
+export const sample_tags = [
+  { name: 'All', count: 6 },
+  { name: 'Comida rapida', count: 4 },
+  { name: 'Pizza', count: 2 },
+  { name: 'Lunch', count: 3 },
+  { name: 'Comida lenta', count: 2 },
+  { name: 'Hamburguesa', count: 1 },
+  { name: 'Frito', count: 1 },
+  { name: 'Sopa', count: 1 },
+];
+
+export const sample_users = [
+  {
+    
+    name: 'Diego',
+    email: 'dbriceno536@gmail.com',
+    password: '12345',
+    address: 'Valera',
+    isAdmin: false,
+  },
+  {
+    
+    name: 'Daniel',
+    email: 'Daniel123@gmail.com',
+    password: '12345',
+    address: 'Valera',
+    isAdmin: true,
+  },
+];
